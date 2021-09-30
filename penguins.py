40a4685c8434996ab303eab3432054c20e4131cd
Dawit Gebeta <dawitgebeta@gmail.com>
2021-09-30 19:23:09 -0400

    prediction table

--- penguins.py
+++ penguins.py
@@ -43,3 +43,44 @@ else:
         features = pd.DataFrame(data, index = [0])
         return features
     input_df = user_input_features()
+
+# Combines user input features with entire penguins dataset
+# This will be useful for the encoding phase
+penguins_raw = pd.read_csv('penguins_cleaned.csv')
+penguins = penguins_raw.drop(columns=['species'])
+df = pd.concat([input_df, penguins], axis = 0)
+
+# Encoding of ordinal features
+# https://www.kaggle.com/pratik1120/penguin-dataset-eda-classification-and-clustering
+encode = ['sex', 'island']
+for col in encode:
+    dummy = pd.get_dummies(df[col], prefix = col)
+    df = pd.concat([df, dummy], axis = 1)
+    del df[col]
+
+df = df[:1] # Selects only the first row (the user input data)
+
+# Displays the user input features
+st.subheader('User Input features')
+
+if upload_file is not None:
+    st.write(df)
+else:
+    st.write('Awaiting CSV file to be uploaded. Currently using example input parameters (shown below).')
+    st.write(df)
+
+# Reads in saved classification model
+load_clf = pickle.load(open('penguins_clf.pkl', 'rb'))
+
+#Apply model to make predictions
+
+prediction = load_clf.predict(df)
+prediction_proba = load_clf.predict_proba(df)
+
+st.subheader('Prediction')
+penguins_species = np.array(['Adelie', 'Chinstrap', 'Gentoo'])
+st.write(penguins_species[prediction])
+
+st.subheader('Prediction Probability')
+st.write(prediction_proba)
+
